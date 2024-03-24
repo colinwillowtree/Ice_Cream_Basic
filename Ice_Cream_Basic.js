a46426a4fdf3c2b55166711a08beadc568ed6a37
Colin Willoughby <39289465+colinwillowtree@users.noreply.github.com>
2024-03-24 12:20:27 -0700

    Update Ice_Cream_Basic.js

--- Ice_Cream_Basic.js
+++ Ice_Cream_Basic.js
@@ -1,11 +1,7 @@
 var icecream = [];
 var lerper;
-var font;
-
-
 
 function preload() {
-
 
   for (i=0; i<30; i++) {
     icecream[i] = loadImage("data/icecream_" + nf(i, 4) + ".png")
@@ -13,6 +9,7 @@ function preload() {
 }
 
 function setup() {
+  
   createCanvas(windowWidth, windowHeight, WEBGL);
   document.body.style.overflow = "hidden";
   imageMode(CENTER);
@@ -20,19 +17,21 @@ function setup() {
   blendMode(BLEND);
   textAlign(CENTER);
   rectMode(CENTER);
+
+  font = loadFont("data/ZTGatha-SemiBold.ttf");
+  textFont(font);
   
 }
 
 
 function draw() {
+  
   clear();
 
   var mapper = map(mouseX, 0, width, 30, 0);
   lerper = lerp(lerper, mapper, 0.1);
   var frame = floor(lerper);
 
-  }
- 
   push();
   image(icecream[frame], 0, 0, width/2, width/2);
   pop();
